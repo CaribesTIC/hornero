4e0c4369013a4c3ffb4ba0baeb202324d6810614
Gregorio Bolivar <gb@jphlions.com>
2017-07-29 18:12:32 -0300

    Limpiando el index de code basura

--- web/index.php
+++ web/index.php
@@ -2,19 +2,8 @@
 
 require '../vendor/autoload.php';
 use JPH\Core\Configuration;
-$init = new Configuration('admin','stable');
+$init = new Configuration('admin','production');
 
-
-//$serverName = "192.168.0.112"; //serverName\instanceName
-//$connectionInfo = array( "Database"=>"Lions_19", "UID"=>"sa", "PWD"=>"Jph135");
-//$conn = sqlsrv_connect( $serverName, $connectionInfo);
-//
-//if( $conn ) {
-//     echo "Conexión establecida.<br />";
-//}else{
-//     echo "Conexión no se pudo establecer.<br />";
-//     die( print_r( sqlsrv_errors(), true));
-//}
 
 
 ?>
